ea6f5ad9d1e149e3137c99ce066a317f01087f70
Luca <luca.cavagnoli@gmail.com>
2021-10-31 15:02:28 +0100

    moved async side effects into action creator

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { DB_URL } from './config/secrets';
-import { uiActions } from './store/ui-slice';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial = true;
 
@@ -18,39 +17,11 @@ function App() {
 
   // replace the whole cart every time it changes:
   useEffect(() => {
-
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data',
-      }));
-      const response = await fetch(DB_URL + '/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      });
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sending cart data succeeded :)',
-      }));
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
-
-    sendCartData().catch((error) => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message:'Sending cart data failed :(',
-      }));
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { DB_URL } from '../config/secrets';
+import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -53,6 +55,47 @@ const cartSlice = createSlice({
     },
   },
 });
+
+/**
+ * action creator
+ */
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending...',
+      message: 'Sending cart data',
+    }));
+
+    const sendRequest = async () => {
+      const response = await fetch(DB_URL + '/cart.json', {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sending cart data succeeded :)',
+      }));
+    }
+    catch (error) {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message:'Sending cart data failed :(',
+      }));
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
